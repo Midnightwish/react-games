874d55d0e0f26769c89ffdc5bf759c63dc432dda
midnightwish <kamil.biz92@gmail.com>
2017-08-07 13:19:41 +0200

    Create game structure and style

--- src/Hangman.js
+++ src/Hangman.js
@@ -3,7 +3,16 @@ import React from 'react';
 const Hangman = () => {
   return (
     <div className = "game">
-      Please, don't hang this guy...
+      <div className = "container">
+        <div id = "password">Hasło</div>
+        <div id = "hangman">
+          <div id = "picture">Obrazek wisielca</div>
+          <div id = "choice-container">
+            <div id = "keyboard">Litery do wyboru</div>
+            <div id = "newgame">Zacznij nową grę!</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
